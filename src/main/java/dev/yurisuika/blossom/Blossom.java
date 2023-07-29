0f9dbead28cacada7363a08d8d8a0a415d659c0f
yurisuika <yurisuika@protonmail.com>
2023-07-29 05:28:43 -0700

    1.1.0 backport

--- src/main/java/dev/yurisuika/blossom/Blossom.java
+++ src/main/java/dev/yurisuika/blossom/Blossom.java
@@ -10,12 +10,12 @@ import net.minecraft.block.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.color.world.BiomeColors;
 import net.minecraft.client.color.world.FoliageColors;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderLayers;
 import net.minecraft.item.*;
-import net.minecraft.tag.TagKey;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -33,6 +33,7 @@ import net.minecraftforge.registries.RegistryObject;
 import java.io.File;
 import java.io.FileWriter;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 @Mod("blossom")
@@ -44,20 +45,155 @@ public class Blossom {
 
     public static class Config {
 
-        public int rate = 5;
-
-        public Count count = new Count(2, 4);
+        public Value value = new Value(
+                new Value.Propagation(0.2F),
+                new Value.Fertilization(0.06666667F),
+                new Value.Pollination(1),
+                new Value.Fruit(3, 0.5714286F)
+        );
+        public Filter filter = new Filter(
+                new Filter.Temperature(-2.0F, 2.0F),
+                new Filter.Downfall(0.0F, 1.0F),
+                new Filter.Dimension(new String[]{"minecraft:overworld"}, new String[]{"minecraft:the_nether", "minecraft:the_end"}),
+                new Filter.Biome(new String[]{"minecraft:forest"}, new String[]{"minecraft:the_void"})
+        );
+        public Toggle toggle = new Toggle(
+                false,
+                false
+        );
 
     }
 
-    public static class Count {
+    public static class Value {
 
-        public int min;
-        public int max;
+        public Propagation propagation;
+        public Fertilization fertilization;
+        public Pollination pollination;
+        public Fruit fruit;
 
-        public Count(int min, int max) {
-            this.min = min;
-            this.max = max;
+        public Value(Propagation propagation, Fertilization fertilization, Pollination pollination, Fruit fruit) {
+            this.propagation = propagation;
+            this.fertilization = fertilization;
+            this.pollination = pollination;
+            this.fruit = fruit;
+        }
+
+        public static class Propagation {
+
+            public float chance;
+
+            public Propagation(float chance) {
+                this.chance = chance;
+            }
+
+        }
+
+        public static class Fertilization {
+
+            public float chance;
+
+            public Fertilization(float chance) {
+                this.chance = chance;
+            }
+
+        }
+
+        public static class Pollination {
+
+            public int age;
+
+            public Pollination(int age) {
+                this.age = age;
+            }
+
+        }
+
+        public static class Fruit {
+
+            public int bonus;
+            public float chance;
+
+            public Fruit(int bonus, float chance) {
+                this.bonus = bonus;
+                this.chance = chance;
+            }
+
+        }
+
+    }
+
+    public static class Filter {
+
+        public Temperature temperature;
+        public Downfall downfall;
+        public Dimension dimension;
+        public Biome biome;
+
+        public Filter(Temperature temperature, Downfall downfall, Dimension dimension, Biome biome) {
+            this.temperature = temperature;
+            this.downfall = downfall;
+            this.dimension = dimension;
+            this.biome = biome;
+        }
+
+        public static class Temperature {
+
+            public float min;
+            public float max;
+
+            public Temperature(float min, float max) {
+                this.min = min;
+                this.max = max;
+            }
+
+        }
+
+        public static class Downfall {
+
+            public float min;
+            public float max;
+
+            public Downfall(float min, float max) {
+                this.min = min;
+                this.max = max;
+            }
+
+        }
+
+        public static class Dimension {
+
+            public String[] whitelist;
+            public String[] blacklist;
+
+            public Dimension(String[] whitelist, String[] blacklist) {
+                this.whitelist = whitelist;
+                this.blacklist = blacklist;
+            }
+
+        }
+
+        public static class Biome {
+
+            public String[] whitelist;
+            public String[] blacklist;
+
+            public Biome(String[] whitelist, String[] blacklist) {
+                this.whitelist = whitelist;
+                this.blacklist = blacklist;
+            }
+
+        }
+
+    }
+
+    public static class Toggle {
+
+        public boolean whitelist;
+        public boolean blacklist;
+
+        public Toggle(boolean whitelist, boolean blacklist) {
+            this.whitelist = whitelist;
+            this.blacklist = blacklist;
         }
 
     }
@@ -95,15 +231,29 @@ public class Blossom {
     }
 
     public static void checkBounds() {
-        int min = Math.max(Math.min(Math.min(config.count.min, 64), config.count.max), 1);
-        int max = Math.max(Math.max(Math.min(config.count.max, 64), config.count.min), 1);
-        config.rate = Math.max(config.rate, 1);
-        config.count.min = min;
-        config.count.max = max;
+        config.value.propagation.chance = Math.max(Math.min(config.value.propagation.chance, 1.0F), 0.0F);
+        config.value.fertilization.chance = Math.max(Math.min(config.value.fertilization.chance, 1.0F), 0.0F);
+        config.value.pollination.age = Math.max(Math.min(config.value.pollination.age, 7), 0);
+        config.value.fruit.bonus = Math.max(config.value.fruit.bonus, 0);
+        config.value.fruit.chance = Math.max(Math.min(config.value.fruit.chance, 1.0F), 0.0F);
+
+        float temperatureMin = Math.max(Math.min(Math.min(config.filter.temperature.min, 2.0F), config.filter.temperature.max), -2.0F);
+        float temperatureMax = Math.max(Math.max(Math.min(config.filter.temperature.max, 2.0F), config.filter.temperature.min), -2.0F);
+        config.filter.temperature.min = temperatureMin;
+        config.filter.temperature.max = temperatureMax;
+
+        float downfallMin = Math.max(Math.min(Math.min(config.filter.downfall.min, 2.0F), config.filter.downfall.max), -2.0F);
+        float downfallMax = Math.max(Math.max(Math.min(config.filter.downfall.max, 2.0F), config.filter.downfall.min), -2.0F);
+        config.filter.downfall.min = downfallMin;
+        config.filter.downfall.max = downfallMax;
+
+        Arrays.sort(config.filter.dimension.whitelist);
+        Arrays.sort(config.filter.dimension.blacklist);
+        Arrays.sort(config.filter.biome.whitelist);
+        Arrays.sort(config.filter.biome.blacklist);
+
         saveConfig();
     }
-
-    public static final TagKey<Block> BLOSSOMS = TagKey.of(Registry.BLOCK_KEY, new Identifier("blossom", "blossoms"));
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, "blossom");
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "blossom");
@@ -112,7 +262,7 @@ public class Blossom {
 
     public static <T extends Block> RegistryObject<T> register(String name, Supplier<T> supplier, Item.Settings settings) {
         RegistryObject<T> block = BLOCKS.register(name, supplier);
-        ITEMS.register(name, () -> new BlockItem(block.get(), settings));
+        ITEMS.register(name, () -> new BlockItem(block.get(), settings.group(ItemGroup.DECORATIONS)));
         return block;
     }
 
@@ -144,6 +294,16 @@ public class Blossom {
         @SubscribeEvent
         public static void clientSetup(FMLClientSetupEvent event) {
             RenderLayers.setRenderLayer(Blossom.FLOWERING_OAK_LEAVES.get(), RenderLayer.getCutout());
+
+            ModelPredicateProviderRegistry.register(FLOWERING_OAK_LEAVES.get().asItem(), new Identifier("age"), (stack, world, entity, seed) -> {
+                NbtCompound nbtCompound = stack.getSubNbt("BlockStateTag");
+                try {
+                    if (nbtCompound != null && nbtCompound.get(FloweringLeavesBlock.AGE.getName()) != null) {
+                        return (float)Integer.parseInt(nbtCompound.get(FloweringLeavesBlock.AGE.getName()).asString()) / 8.0F;
+                    }
+                } catch (NumberFormatException ignored) {}
+                return 0.0F;
+            });
         }
 
         @SubscribeEvent
