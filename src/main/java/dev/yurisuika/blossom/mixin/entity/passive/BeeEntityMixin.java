26b75c72e366dc15d8bbbbfef69d457e9891ee9a
yurisuika <yurisuika@protonmail.com>
2023-07-29 06:55:37 -0700

    shouldn't have done that now should we

--- src/main/java/dev/yurisuika/blossom/mixin/entity/passive/BeeEntityMixin.java
+++ src/main/java/dev/yurisuika/blossom/mixin/entity/passive/BeeEntityMixin.java
@@ -36,8 +36,8 @@ public class BeeEntityMixin {
         public BeeEntity entity;
 
         @Inject(method = "<init>", at = @At(value = "TAIL"))
-        private void injectInit(BeeEntity entity, CallbackInfo ci) {
-            entity = entity;
+        private void injectInit(BeeEntity entityType, CallbackInfo ci) {
+            entity = entityType;
         }
 
         @Inject(method = "tick", at = @At(value = "HEAD"))
