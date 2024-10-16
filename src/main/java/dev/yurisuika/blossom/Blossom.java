491f4774e5daff0896059a62ee913f33220dd1d5
yurisuika <yurisuika@protonmail.com>
2024-10-15 17:00:33 -0700

    mojmap

--- src/main/java/dev/yurisuika/blossom/Blossom.java
+++ src/main/java/dev/yurisuika/blossom/Blossom.java
@@ -1,12 +1,14 @@
 package dev.yurisuika.blossom;
 
-import dev.yurisuika.blossom.block.FloweringLeavesBlock;
-import dev.yurisuika.blossom.block.FruitingLeavesBlock;
 import dev.yurisuika.blossom.client.particle.BlossomParticle;
-import dev.yurisuika.blossom.entity.ai.goal.BlossomGoal;
-import dev.yurisuika.blossom.entity.ai.goal.FruitGoal;
-import dev.yurisuika.blossom.mixin.block.BlocksInvoker;
-import dev.yurisuika.blossom.server.command.BlossomCommand;
+import dev.yurisuika.blossom.mixin.world.level.block.BlocksInvoker;
+import dev.yurisuika.blossom.server.commands.BlossomCommand;
+import dev.yurisuika.blossom.util.Validate;
+import dev.yurisuika.blossom.util.config.Config;
+import dev.yurisuika.blossom.world.entity.ai.goal.BlossomGoal;
+import dev.yurisuika.blossom.world.entity.ai.goal.FruitGoal;
+import dev.yurisuika.blossom.world.level.block.FloweringLeavesBlock;
+import dev.yurisuika.blossom.world.level.block.FruitingLeavesBlock;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -20,55 +22,60 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
-import net.minecraft.block.*;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.color.world.BiomeColors;
-import net.minecraft.client.color.world.FoliageColors;
-import net.minecraft.client.item.ModelPredicateProviderRegistry;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.entity.passive.BeeEntity;
-import net.minecraft.item.*;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.particle.DefaultParticleType;
-import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.core.Registry;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.animal.Bee;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.FoliageColor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.Material;
 
-import static dev.yurisuika.blossom.client.option.BlossomConfig.*;
+import java.util.Objects;
 
-public class Blossom implements ModInitializer, ClientModInitializer {
+public class Blossom implements ModInitializer {
 
     public static final Block FRUITING_OAK_LEAVES = new FruitingLeavesBlock(Blocks.OAK_LEAVES, Items.APPLE, FabricBlockSettings.of(Material.LEAVES)
             .strength(0.2F)
             .ticksRandomly()
-            .sounds(BlockSoundGroup.GRASS)
+            .sounds(SoundType.GRASS)
             .nonOpaque()
-            .allowsSpawning(BlocksInvoker::invokeCanSpawnOnLeaves)
+            .allowsSpawning(BlocksInvoker::invokeOcelotOrParrot)
             .suffocates(BlocksInvoker::invokeNever)
             .blockVision(BlocksInvoker::invokeNever));
     public static final Block FLOWERING_OAK_LEAVES = new FloweringLeavesBlock(Blocks.OAK_LEAVES, Blossom.FRUITING_OAK_LEAVES, FabricBlockSettings.of(Material.LEAVES)
             .strength(0.2F)
             .ticksRandomly()
-            .sounds(BlockSoundGroup.GRASS)
+            .sounds(SoundType.GRASS)
             .nonOpaque()
-            .allowsSpawning(BlocksInvoker::invokeCanSpawnOnLeaves)
+            .allowsSpawning(BlocksInvoker::invokeOcelotOrParrot)
             .suffocates(BlocksInvoker::invokeNever)
             .blockVision(BlocksInvoker::invokeNever));
 
-    public static final DefaultParticleType BLOSSOM = FabricParticleTypes.simple(false);
+    public static final SimpleParticleType BLOSSOM = FabricParticleTypes.simple(false);
 
     public static void registerBlocks() {
-        Registry.register(Registry.BLOCK, new Identifier("blossom", "flowering_oak_leaves"), FLOWERING_OAK_LEAVES);
-        Registry.register(Registry.BLOCK, new Identifier("blossom", "fruiting_oak_leaves"), FRUITING_OAK_LEAVES);
+        Registry.register(Registry.BLOCK, new ResourceLocation("blossom", "flowering_oak_leaves"), FLOWERING_OAK_LEAVES);
+        Registry.register(Registry.BLOCK, new ResourceLocation("blossom", "fruiting_oak_leaves"), FRUITING_OAK_LEAVES);
     }
 
     public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier("blossom", "flowering_oak_leaves"), new BlockItem(FLOWERING_OAK_LEAVES, new Item.Settings().group(ItemGroup.DECORATIONS)));
-        Registry.register(Registry.ITEM, new Identifier("blossom", "fruiting_oak_leaves"), new BlockItem(FRUITING_OAK_LEAVES, new Item.Settings().group(ItemGroup.DECORATIONS)));
+        Registry.register(Registry.ITEM, new ResourceLocation("blossom", "flowering_oak_leaves"), new BlockItem(FLOWERING_OAK_LEAVES, new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
+        Registry.register(Registry.ITEM, new ResourceLocation("blossom", "fruiting_oak_leaves"), new BlockItem(FRUITING_OAK_LEAVES, new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
     }
 
     public static void registerParticles() {
-        Registry.register(Registry.PARTICLE_TYPE, new Identifier("blossom", "blossom"), BLOSSOM);
+        Registry.register(Registry.PARTICLE_TYPE, new ResourceLocation("blossom", "blossom"), BLOSSOM);
     }
 
     public static void registerCompostables() {
@@ -82,10 +89,10 @@ public class Blossom implements ModInitializer, ClientModInitializer {
     }
 
     public static void registerServerEntityEvents() {
-        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
-            if (entity instanceof BeeEntity) {
-                ((BeeEntity)entity).getGoalSelector().add(4, new BlossomGoal((BeeEntity)entity));
-                ((BeeEntity)entity).getGoalSelector().add(4, new FruitGoal((BeeEntity)entity));
+        ServerEntityEvents.ENTITY_LOAD.register((entity, level) -> {
+            if (entity instanceof Bee) {
+                ((Bee) entity).getGoalSelector().addGoal(4, new BlossomGoal((Bee) entity));
+                ((Bee) entity).getGoalSelector().addGoal(4, new FruitGoal((Bee) entity));
             }
         });
     }
@@ -94,54 +101,10 @@ public class Blossom implements ModInitializer, ClientModInitializer {
         CommandRegistrationCallback.EVENT.register(BlossomCommand::register);
     }
 
-    @Environment(EnvType.CLIENT)
-    public static void registerParticleFactories() {
-        ParticleFactoryRegistry.getInstance().register(BLOSSOM, BlossomParticle.Factory::new);
-    }
-
-    @Environment(EnvType.CLIENT)
-    public static void registerRenderLayers() {
-        BlockRenderLayerMap.INSTANCE.putBlock(FLOWERING_OAK_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FRUITING_OAK_LEAVES, RenderLayer.getCutout());
-    }
-
-    @Environment(EnvType.CLIENT)
-    public static void registerColorProviders() {
-        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.getColor(0.5, 1.0), FLOWERING_OAK_LEAVES);
-        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.getColor(0.5, 1.0), FRUITING_OAK_LEAVES);
-
-        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex > 0 ? -1 : MinecraftClient.getInstance().getBlockColors().getColor(((BlockItem) stack.getItem()).getBlock().getDefaultState(), null, null, tintIndex), FLOWERING_OAK_LEAVES);
-        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex > 0 ? -1 : MinecraftClient.getInstance().getBlockColors().getColor(((BlockItem) stack.getItem()).getBlock().getDefaultState(), null, null, tintIndex), FRUITING_OAK_LEAVES);
-    }
-
-    @Environment(EnvType.CLIENT)
-    public static void registerModelPredicateProviders() {
-        ModelPredicateProviderRegistry.register(FLOWERING_OAK_LEAVES.asItem(), new Identifier("age"), (stack, world, entity, seed) -> {
-            NbtCompound nbtCompound = stack.getSubNbt("BlockStateTag");
-            try {
-                if (nbtCompound != null && nbtCompound.get(FloweringLeavesBlock.AGE.getName()) != null) {
-                    return (float)Integer.parseInt(nbtCompound.get(FloweringLeavesBlock.AGE.getName()).asString()) / 4.0F;
-                }
-            } catch (NumberFormatException ignored) {}
-            return 0.0F;
-        });
-        ModelPredicateProviderRegistry.register(FRUITING_OAK_LEAVES.asItem(), new Identifier("age"), (stack, world, entity, seed) -> {
-            NbtCompound nbtCompound = stack.getSubNbt("BlockStateTag");
-            try {
-                if (nbtCompound != null && nbtCompound.get(FruitingLeavesBlock.AGE.getName()) != null) {
-                    return (float)Integer.parseInt(nbtCompound.get(FruitingLeavesBlock.AGE.getName()).asString()) / 8.0F;
-                }
-            } catch (NumberFormatException ignored) {}
-            return 0.0F;
-        });
-    }
-
     @Override
     public void onInitialize() {
-        if (!file.exists()) {
-            saveConfig();
-        }
-        loadConfig();
+        Config.loadConfig();
+        Validate.checkBounds();
 
         registerBlocks();
         registerItems();
@@ -152,12 +115,58 @@ public class Blossom implements ModInitializer, ClientModInitializer {
         registerCommands();
     }
 
-    @Override
-    public void onInitializeClient() {
-        registerParticleFactories();
-        registerRenderLayers();
-        registerColorProviders();
-        registerModelPredicateProviders();
+    public static class Client implements ClientModInitializer {
+
+        @Environment(EnvType.CLIENT)
+        public static void registerParticleFactories() {
+            ParticleFactoryRegistry.getInstance().register(BLOSSOM, BlossomParticle.Factory::new);
+        }
+
+        @Environment(EnvType.CLIENT)
+        public static void registerRenderLayers() {
+            BlockRenderLayerMap.INSTANCE.putBlock(FLOWERING_OAK_LEAVES, RenderType.cutout());
+            BlockRenderLayerMap.INSTANCE.putBlock(FRUITING_OAK_LEAVES, RenderType.cutout());
+        }
+
+        @Environment(EnvType.CLIENT)
+        public static void registerColorProviders() {
+            ColorProviderRegistry.BLOCK.register((state, level, pos, tintIndex) -> level != null && pos != null ? BiomeColors.getAverageFoliageColor(level, pos) : FoliageColor.get(0.5F, 1.0F), FLOWERING_OAK_LEAVES);
+            ColorProviderRegistry.BLOCK.register((state, level, pos, tintIndex) -> level != null && pos != null ? BiomeColors.getAverageFoliageColor(level, pos) : FoliageColor.get(0.5F, 1.0F), FRUITING_OAK_LEAVES);
+
+            ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex > 0 ? -1 : Minecraft.getInstance().getBlockColors().getColor(((BlockItem) stack.getItem()).getBlock().defaultBlockState(), null, null, tintIndex), FLOWERING_OAK_LEAVES);
+            ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex > 0 ? -1 : Minecraft.getInstance().getBlockColors().getColor(((BlockItem) stack.getItem()).getBlock().defaultBlockState(), null, null, tintIndex), FRUITING_OAK_LEAVES);
+        }
+
+        @Environment(EnvType.CLIENT)
+        public static void registerModelPredicateProviders() {
+            ItemProperties.register(FLOWERING_OAK_LEAVES.asItem(), new ResourceLocation("age"), (stack, world, entity, seed) -> {
+                CompoundTag tag = stack.getTagElement("BlockStateTag");
+                try {
+                    if (Objects.nonNull(tag) && Objects.nonNull(tag.get(FloweringLeavesBlock.AGE.getName()))) {
+                        return Integer.parseInt(tag.get(FloweringLeavesBlock.AGE.getName()).getAsString()) / 4.0F;
+                    }
+                } catch (NumberFormatException ignored) {}
+                return 0.0F;
+            });
+            ItemProperties.register(FRUITING_OAK_LEAVES.asItem(), new ResourceLocation("age"), (stack, world, entity, seed) -> {
+                CompoundTag tag = stack.getTagElement("BlockStateTag");
+                try {
+                    if (Objects.nonNull(tag) && Objects.nonNull(tag.get(FruitingLeavesBlock.AGE.getName()))) {
+                        return Integer.parseInt(tag.get(FruitingLeavesBlock.AGE.getName()).getAsString()) / 8.0F;
+                    }
+                } catch (NumberFormatException ignored) {}
+                return 0.0F;
+            });
+        }
+
+        @Override
+        public void onInitializeClient() {
+            registerParticleFactories();
+            registerRenderLayers();
+            registerColorProviders();
+            registerModelPredicateProviders();
+        }
+
     }
 
 }
---
+++ src/main/java/dev/yurisuika/blossom/world/level/block/FloweringLeavesBlock.java
@@ -0,0 +1,251 @@
+package dev.yurisuika.blossom.world.level.block;
+
+import dev.yurisuika.blossom.Blossom;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.BlockPos.MutableBlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShearsItem;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.biome.Biome.Precipitation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition.Builder;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import java.util.OptionalInt;
+import java.util.Random;
+
+public class FloweringLeavesBlock extends LeavesBlock implements BonemealableBlock {
+
+    public final Block shearedBlock;
+    public final Block pollinatedBlock;
+    public static final IntegerProperty DISTANCE =  BlockStateProperties.DISTANCE;
+    public static final BooleanProperty PERSISTENT = BlockStateProperties.PERSISTENT;
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_3;
+    public static final IntegerProperty RIPENESS = IntegerProperty.create("ripeness", 0, 7);
+
+    public FloweringLeavesBlock(Block shearedBlock, Block pollinatedBlock, Properties properties) {
+        super(properties);
+        this.shearedBlock = shearedBlock;
+        this.pollinatedBlock = pollinatedBlock;
+        registerDefaultState(stateDefinition.any().setValue(DISTANCE, 1).setValue(PERSISTENT, false).setValue(AGE, 0).setValue(RIPENESS, 0));
+    }
+
+    public VoxelShape getBlockSupportShape(BlockState state, BlockGetter level, BlockPos pos) {
+        return Shapes.empty();
+    }
+
+    public boolean isRandomlyTicking(BlockState state) {
+        return true;
+    }
+
+    public IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    public int getMaxAge() {
+        return 3;
+    }
+
+    public int getAge(BlockState state) {
+        return state.getValue(getAgeProperty());
+    }
+
+    public boolean isMaxAge(BlockState state) {
+        return state.getValue(getAgeProperty()) >= getMaxAge();
+    }
+
+    public IntegerProperty getRipenessProperty() {
+        return RIPENESS;
+    }
+
+    public int getMaxRipeness() {
+        return 7;
+    }
+
+    public int getRipeness(BlockState state) {
+        return  state.getValue(getRipenessProperty());
+    }
+
+    public boolean isRipe(BlockState state) {
+        return state.getValue(getRipenessProperty()) >= getMaxRipeness();
+    }
+
+    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, Random random) {
+        if (!state.getValue(PERSISTENT) && state.getValue(DISTANCE) == 7) {
+            dropResources(state, level, pos);
+            level.removeBlock(pos, false);
+        } else if (!isMaxAge(state) && level.getRawBrightness(pos, 0) >= 9) {
+            int i = getAge(state);
+            if (i < getMaxAge()) {
+                float temperature = level.getBiome(pos).value().getBaseTemperature();
+                float downfall = level.getBiome(pos).value().getDownfall();
+                temperature += 2;
+                float f = (downfall * temperature) / 4;
+                f = ((4 - 1) * f) + 1;
+                Precipitation precipitation = level.getBiome(pos).value().getPrecipitation();
+                if (level.isRaining() && precipitation == Precipitation.RAIN) {
+                    f = 5.0F;
+                }
+                if (random.nextInt((int)(25.0F / f) + 1) == 0) {
+                    level.setBlock(pos, defaultBlockState().setValue(AGE, i + 1)
+                            .setValue(DISTANCE, state.getValue(DISTANCE))
+                            .setValue(PERSISTENT, state.getValue(PERSISTENT))
+                            .setValue(RIPENESS, state.getValue(RIPENESS)), 2);
+                }
+            }
+        } else if (isMaxAge(state)) {
+            int i = getRipeness(state);
+            if (i < getMaxRipeness()) {
+                if (random.nextInt((int)(25.0F) + 1) == 0) {
+                    level.setBlock(pos, defaultBlockState().setValue(RIPENESS, i + 1), 2);
+                }
+            }
+        }
+        if (isRipe(state)) {
+            level.setBlockAndUpdate(pos, shearedBlock.defaultBlockState()
+                    .setValue(DISTANCE, state.getValue(DISTANCE))
+                    .setValue(PERSISTENT, state.getValue(PERSISTENT))
+            );
+        }
+        if (!isMaxAge(state) && state.getValue(RIPENESS) > 0) {
+            level.setBlock(pos, defaultBlockState().setValue(RIPENESS, 0), 2);
+        }
+    }
+
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, Random random) {
+        level.setBlock(pos, updateDistance(state, level, pos), 3);
+    }
+
+    public void applyGrowth(Level level, BlockPos pos, BlockState state) {
+        int i = getAge(state) + getGrowthAmount(level);
+        int j = getMaxAge();
+        if (i > j) {
+            i = j;
+        }
+        level.setBlock(pos, defaultBlockState().setValue(AGE, i)
+                .setValue(DISTANCE, state.getValue(DISTANCE))
+                .setValue(PERSISTENT, state.getValue(PERSISTENT))
+                .setValue(RIPENESS, state.getValue(RIPENESS)), 2);
+    }
+
+    public int getGrowthAmount(Level level) {
+        return Mth.nextInt(level.getRandom(), 2, 5);
+    }
+
+    public int getLightBlock(BlockState state, BlockGetter level, BlockPos pos) {
+        return 1;
+    }
+
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+        int i = getDistanceFromLog(neighborState) + 1;
+        if (i != 1 || state.getValue(DISTANCE) != i) {
+            level.scheduleTick(pos, this, 1);
+        }
+        return state;
+    }
+
+    public static BlockState updateDistance(BlockState state, LevelAccessor level, BlockPos pos) {
+        int i = 7;
+        MutableBlockPos mutableBlockPos = new MutableBlockPos();
+        for (Direction direction : Direction.values()) {
+            mutableBlockPos.setWithOffset(pos, direction);
+            i = Math.min(i, getDistanceFromLog(level.getBlockState(mutableBlockPos)) + 1);
+            if (i == 1) {
+                break;
+            }
+        }
+        return state.setValue(DISTANCE, i);
+    }
+
+    public static int getDistanceFromLog(BlockState state) {
+        return getOptionalDistanceFromLog(state).orElse(7);
+    }
+
+    public static OptionalInt getOptionalDistanceFromLog(BlockState state) {
+        if (state.is(BlockTags.LOGS)) {
+            return OptionalInt.of(0);
+        }
+        if (state.hasProperty(LeavesBlock.DISTANCE)) {
+            return OptionalInt.of(state.getValue(DISTANCE));
+        }
+        return OptionalInt.empty();
+    }
+
+    public void animateTick(BlockState state, Level level, BlockPos pos, Random random) {
+        super.animateTick(state, level, pos, random);
+        if (random.nextInt(10) != 0) {
+            return;
+        }
+        if (FloweringLeavesBlock.isFaceFull(level.getBlockState(pos.below()).getCollisionShape(level, pos.below()), Direction.UP)) {
+            return;
+        }
+        double d = pos.getX() + random.nextDouble();
+        double e = pos.getY() - 0.05D;
+        double f = pos.getZ() + random.nextDouble();
+        level.addParticle(Blossom.BLOSSOM, d, e, f, 0.0D, 0.0D, 0.0D);
+    }
+
+    public void createBlockStateDefinition(Builder<Block, BlockState> builder) {
+        builder.add(DISTANCE, PERSISTENT, AGE, RIPENESS);
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return updateDistance(defaultBlockState().setValue(PERSISTENT, true).setValue(AGE, 0).setValue(RIPENESS, 0), context.getLevel(), context.getClickedPos());
+    }
+
+    public boolean isValidBonemealTarget(BlockGetter level, BlockPos pos, BlockState state, boolean isClient) {
+        return !isMaxAge(state);
+    }
+
+    public boolean isBonemealSuccess(Level level, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    public void performBonemeal(ServerLevel level, Random random, BlockPos pos, BlockState state) {
+        applyGrowth(level, pos, state);
+    }
+
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        ItemStack itemStack = player.getItemInHand(hand);
+        Item item = itemStack.getItem();
+        if (item instanceof ShearsItem) {
+            level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.CROP_BREAK, SoundSource.NEUTRAL, 1.0F, 1.0F);
+            itemStack.hurtAndBreak(1, player, entity -> entity.broadcastBreakEvent(hand));
+            if (!level.isClientSide()) {
+                player.awardStat(Stats.ITEM_USED.get(item));
+            }
+            level.gameEvent(player, GameEvent.SHEAR, pos);
+            level.setBlockAndUpdate(pos, shearedBlock.defaultBlockState()
+                    .setValue(DISTANCE, state.getValue(DISTANCE))
+                    .setValue(PERSISTENT, state.getValue(PERSISTENT))
+            );
+            return InteractionResult.SUCCESS;
+        } else {
+            return InteractionResult.PASS;
+        }
+    }
+
+}
