27f875298de5304cb522fa53f2b762c128104238
yurisuika <yurisuika@protonmail.com>
2024-10-27 22:29:52 -0700

    1.21.3

--- src/main/java/dev/yurisuika/blossom/world/level/block/FloweringLeavesBlock.java
+++ src/main/java/dev/yurisuika/blossom/world/level/block/FloweringLeavesBlock.java
@@ -14,7 +14,7 @@ import net.minecraft.util.Mth;
 import net.minecraft.util.ParticleUtils;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -118,7 +118,7 @@ public class FloweringLeavesBlock extends LeavesBlock implements BonemealableBlo
                 temperature += 2;
                 float f = (downfall * temperature) / 4;
                 f = ((4 - 1) * f) + 1;
-                Precipitation precipitation = level.getBiome(pos).value().getPrecipitationAt(pos);
+                Precipitation precipitation = level.getBiome(pos).value().getPrecipitationAt(pos, level.getSeaLevel());
                 if (level.isRaining() && precipitation == Precipitation.RAIN) {
                     f = 5.0F;
                 }
@@ -248,12 +248,11 @@ public class FloweringLeavesBlock extends LeavesBlock implements BonemealableBlo
         applyGrowth(level, pos, state);
     }
 
-    public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        ItemStack itemStack = player.getItemInHand(hand);
-        Item item = itemStack.getItem();
+    public InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        Item item = stack.getItem();
         if (item instanceof ShearsItem) {
             level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.CROP_BREAK, SoundSource.NEUTRAL, 1.0F, 1.0F);
-            itemStack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(hand));
+            stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(hand));
             if (!level.isClientSide()) {
                 player.awardStat(Stats.ITEM_USED.get(item));
             }
@@ -263,9 +262,9 @@ public class FloweringLeavesBlock extends LeavesBlock implements BonemealableBlo
                     .setValue(PERSISTENT, state.getValue(PERSISTENT))
                     .setValue(WATERLOGGED, state.getValue(WATERLOGGED))
             );
-            return ItemInteractionResult.SUCCESS;
+            return InteractionResult.SUCCESS;
         } else {
-            return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+            return InteractionResult.PASS;
         }
     }
 
--- src/main/java/dev/yurisuika/blossom/world/level/block/FruitingLeavesBlock.java
+++ src/main/java/dev/yurisuika/blossom/world/level/block/FruitingLeavesBlock.java
@@ -14,7 +14,7 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -23,10 +23,7 @@ import net.minecraft.world.item.ShearsItem;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.*;
 import net.minecraft.world.level.biome.Biome.Precipitation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BonemealableBlock;
@@ -121,7 +118,7 @@ public class FruitingLeavesBlock extends LeavesBlock implements BonemealableBloc
                 temperature += 2;
                 float f = (downfall * temperature) / 4;
                 f = ((4 - 1) * f) + 1;
-                Precipitation precipitation = level.getBiome(pos).value().getPrecipitationAt(pos);
+                Precipitation precipitation = level.getBiome(pos).value().getPrecipitationAt(pos, level.getSeaLevel());
                 if (level.isRaining() && precipitation == Precipitation.RAIN) {
                     f = 5.0F;
                 }
@@ -175,17 +172,17 @@ public class FruitingLeavesBlock extends LeavesBlock implements BonemealableBloc
         return Mth.nextInt(level.getRandom(), 2, 5);
     }
 
-    public int getLightBlock(BlockState state, BlockGetter level, BlockPos pos) {
+    public int getLightBlock(BlockState state) {
         return 1;
     }
 
-    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+    public BlockState updateShape(BlockState state, LevelReader level, ScheduledTickAccess scheduledTickAccess, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, RandomSource randomSource) {
         if (state.getValue(WATERLOGGED)) {
-            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+            scheduledTickAccess.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
         }
         int i = getDistanceFromLog(neighborState) + 1;
         if (i != 1 || state.getValue(DISTANCE) != i) {
-            level.scheduleTick(pos, this, 1);
+            scheduledTickAccess.scheduleTick(pos, this, 1);
         }
         return state;
     }
@@ -255,14 +252,13 @@ public class FruitingLeavesBlock extends LeavesBlock implements BonemealableBloc
         popResource(level, pos, new ItemStack(item, count));
     }
 
-    public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        ItemStack itemStack = player.getItemInHand(hand);
+    public InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
         if (isMaxAge(state)) {
-            Item item = itemStack.getItem();
+            Item item = stack.getItem();
             if (item instanceof ShearsItem) {
                 level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.CROP_BREAK, SoundSource.NEUTRAL, 1.0F, 1.0F);
-                dropFruit(level, pos, shearedItem, (itemStack.isEnchanted() && EnchantmentHelper.getEnchantmentsForCrafting(itemStack).entrySet().contains(Enchantments.FORTUNE)) ? EnchantmentHelper.getItemEnchantmentLevel(level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.FORTUNE), itemStack) : 0);
-                itemStack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(hand));
+                dropFruit(level, pos, shearedItem, (stack.isEnchanted() && EnchantmentHelper.getEnchantmentsForCrafting(stack).entrySet().contains(Enchantments.FORTUNE)) ? EnchantmentHelper.getItemEnchantmentLevel(level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.FORTUNE), stack) : 0);
+                stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(hand));
                 if (!level.isClientSide()) {
                     player.awardStat(Stats.ITEM_USED.get(item));
                 }
@@ -273,9 +269,9 @@ public class FruitingLeavesBlock extends LeavesBlock implements BonemealableBloc
                         .setValue(WATERLOGGED, state.getValue(WATERLOGGED))
                 );
             }
-            return ItemInteractionResult.SUCCESS;
+            return InteractionResult.SUCCESS;
         } else {
-            return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+            return InteractionResult.PASS;
         }
     }
 
