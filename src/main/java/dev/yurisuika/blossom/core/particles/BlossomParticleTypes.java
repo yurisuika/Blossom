2d232b8cf46336009ed0d7a9680df7c169c69548
yurisuika <yurisuika@protonmail.com>
2024-12-11 10:44:34 -0800

    Changes far too numerous to enumerate.

---
+++ src/main/java/dev/yurisuika/blossom/core/particles/BlossomParticleTypes.java
@@ -0,0 +1,20 @@
+package dev.yurisuika.blossom.core.particles;
+
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class BlossomParticleTypes {
+
+    public static final DeferredRegister<ParticleType<?>> PARTICLES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, "blossom");
+
+    public static final RegistryObject<SimpleParticleType> FLOWERING_OAK_LEAVES = PARTICLES.register("flowering_oak_leaves", () -> new SimpleParticleType(false));
+
+    public static void register(IEventBus eventBus) {
+        PARTICLES.register(eventBus);
+    }
+
+}
