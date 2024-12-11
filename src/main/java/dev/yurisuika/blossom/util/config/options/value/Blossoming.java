2a94691ef06b832ca295e5ddb8432e096a7ba495
yurisuika <yurisuika@protonmail.com>
2024-12-11 10:45:16 -0800

    Changes far too numerous to enumerate.

--- src/main/java/dev/yurisuika/blossom/util/config/options/value/Blossoming.java
+++ src/main/java/dev/yurisuika/blossom/util/config/options/value/Blossoming.java
@@ -3,11 +3,9 @@ package dev.yurisuika.blossom.util.config.options.value;
 public class Blossoming {
 
     public float chance;
-    public double distance;
 
-    public Blossoming(float chance, double distance) {
+    public Blossoming(float chance) {
         this.chance = chance;
-        this.distance = distance;
     }
 
     public float getChance() {
@@ -16,14 +14,6 @@ public class Blossoming {
 
     public void setChance(float chance) {
         this.chance = chance;
-    }
-
-    public double getDistance() {
-        return distance;
-    }
-
-    public void setDistance(double distance) {
-        this.distance = distance;
     }
     
 }
