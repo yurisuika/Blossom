3079eec528a16b52291a62418b0745d0f5fe528c
yurisuika <yurisuika@protonmail.com>
2024-12-27 04:16:12 -0800

    rework flowering leaves, registration

--- src/main/java/dev/yurisuika/blossom/world/level/block/BlossomBlocks.java
+++ src/main/java/dev/yurisuika/blossom/world/level/block/BlossomBlocks.java
@@ -1,46 +1,38 @@
 package dev.yurisuika.blossom.world.level.block;
 
 import dev.yurisuika.blossom.core.particles.BlossomParticleTypes;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
+import dev.yurisuika.blossom.mixin.world.level.block.BlocksInvoker;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
 
 public class BlossomBlocks {
 
-    public static final Block FRUITING_OAK_LEAVES = new FruitingLeavesBlock(FabricBlockSettings.create()
+    public static final Block FLOWERING_OAK_LEAVES = new FloweringLeavesBlock(BlossomParticleTypes.FLOWERING_OAK_LEAVES, BlockBehaviour.Properties.of()
             .mapColor(MapColor.PLANT)
             .strength(0.2F)
-            .ticksRandomly()
-            .sounds(SoundType.GRASS)
-            .nonOpaque()
-            .allowsSpawning(Blocks::ocelotOrParrot)
-            .suffocates(Blocks::never)
-            .blockVision(Blocks::never)
-            .burnable()
-            .pistonBehavior(PushReaction.DESTROY)
-            .solidBlock(Blocks::never));
-    public static final Block FLOWERING_OAK_LEAVES = new FloweringLeavesBlock(BlossomParticleTypes.FLOWERING_OAK_LEAVES, FabricBlockSettings.create()
+            .randomTicks()
+            .sound(SoundType.GRASS)
+            .noOcclusion()
+            .isValidSpawn(BlocksInvoker::invokeOcelotOrParrot)
+            .isSuffocating(BlocksInvoker::invokeNever)
+            .isViewBlocking(BlocksInvoker::invokeNever)
+            .ignitedByLava()
+            .pushReaction(PushReaction.DESTROY)
+            .isRedstoneConductor(BlocksInvoker::invokeNever));
+    public static final Block FRUITING_OAK_LEAVES = new FruitingLeavesBlock(BlockBehaviour.Properties.of()
             .mapColor(MapColor.PLANT)
             .strength(0.2F)
-            .ticksRandomly()
-            .sounds(SoundType.GRASS)
-            .nonOpaque()
-            .allowsSpawning(Blocks::ocelotOrParrot)
-            .suffocates(Blocks::never)
-            .blockVision(Blocks::never)
-            .burnable()
-            .pistonBehavior(PushReaction.DESTROY)
-            .solidBlock(Blocks::never));
-
-    public static void register() {
-        Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.tryParse("blossom:fruiting_oak_leaves"), FRUITING_OAK_LEAVES);
-        Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.tryParse("blossom:flowering_oak_leaves"), FLOWERING_OAK_LEAVES);
-    }
+            .randomTicks()
+            .sound(SoundType.GRASS)
+            .noOcclusion()
+            .isValidSpawn(BlocksInvoker::invokeOcelotOrParrot)
+            .isSuffocating(BlocksInvoker::invokeNever)
+            .isViewBlocking(BlocksInvoker::invokeNever)
+            .ignitedByLava()
+            .pushReaction(PushReaction.DESTROY)
+            .isRedstoneConductor(BlocksInvoker::invokeNever));
 
 }
