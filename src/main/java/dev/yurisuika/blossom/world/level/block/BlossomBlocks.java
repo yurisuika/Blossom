0d07666e7f0ea2aefa2f305b87cbf35a868ab33c
yurisuika <yurisuika@protonmail.com>
2024-12-27 04:15:29 -0800

    rework flowering leaves, registration

--- src/main/java/dev/yurisuika/blossom/world/level/block/BlossomBlocks.java
+++ src/main/java/dev/yurisuika/blossom/world/level/block/BlossomBlocks.java
@@ -2,35 +2,28 @@ package dev.yurisuika.blossom.world.level.block;
 
 import dev.yurisuika.blossom.core.particles.BlossomParticleTypes;
 import dev.yurisuika.blossom.mixin.world.level.block.BlocksInvoker;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 
 public class BlossomBlocks {
 
-    public static final Block FRUITING_OAK_LEAVES = new FruitingLeavesBlock(FabricBlockSettings.of(Material.LEAVES)
+    public static final Block FLOWERING_OAK_LEAVES = new FloweringLeavesBlock(BlossomParticleTypes.FLOWERING_OAK_LEAVES, BlockBehaviour.Properties.of(Material.LEAVES)
             .strength(0.2F)
-            .ticksRandomly()
-            .sounds(SoundType.GRASS)
-            .nonOpaque()
-            .allowsSpawning(BlocksInvoker::invokeOcelotOrParrot)
-            .suffocates(BlocksInvoker::invokeNever)
-            .blockVision(BlocksInvoker::invokeNever));
-    public static final Block FLOWERING_OAK_LEAVES = new FloweringLeavesBlock(BlossomParticleTypes.FLOWERING_OAK_LEAVES, FabricBlockSettings.of(Material.LEAVES)
+            .randomTicks()
+            .sound(SoundType.GRASS)
+            .noOcclusion()
+            .isValidSpawn(BlocksInvoker::invokeOcelotOrParrot)
+            .isSuffocating(BlocksInvoker::invokeNever)
+            .isViewBlocking(BlocksInvoker::invokeNever));
+    public static final Block FRUITING_OAK_LEAVES = new FruitingLeavesBlock(BlockBehaviour.Properties.of(Material.LEAVES)
             .strength(0.2F)
-            .ticksRandomly()
-            .sounds(SoundType.GRASS)
-            .nonOpaque()
-            .allowsSpawning(BlocksInvoker::invokeOcelotOrParrot)
-            .suffocates(BlocksInvoker::invokeNever)
-            .blockVision(BlocksInvoker::invokeNever));
-
-    public static void register() {
-        Registry.register(Registry.BLOCK, ResourceLocation.tryParse("blossom:fruiting_oak_leaves"), FRUITING_OAK_LEAVES);
-        Registry.register(Registry.BLOCK, ResourceLocation.tryParse("blossom:flowering_oak_leaves"), FLOWERING_OAK_LEAVES);
-    }
+            .randomTicks()
+            .sound(SoundType.GRASS)
+            .noOcclusion()
+            .isValidSpawn(BlocksInvoker::invokeOcelotOrParrot)
+            .isSuffocating(BlocksInvoker::invokeNever)
+            .isViewBlocking(BlocksInvoker::invokeNever));
 
 }
