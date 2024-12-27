a9811f753370ad0a51fb2560d029a7778fd31acb
yurisuika <yurisuika@protonmail.com>
2024-12-27 04:15:41 -0800

    rework flowering leaves, registration

--- src/main/java/dev/yurisuika/blossom/core/particles/BlossomParticleTypes.java
+++ src/main/java/dev/yurisuika/blossom/core/particles/BlossomParticleTypes.java
@@ -1,16 +1,10 @@
 package dev.yurisuika.blossom.core.particles;
 
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
-import net.minecraft.core.Registry;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraft.resources.ResourceLocation;
 
 public class BlossomParticleTypes {
 
     public static final SimpleParticleType FLOWERING_OAK_LEAVES = FabricParticleTypes.simple(false);
-
-    public static void register() {
-        Registry.register(Registry.PARTICLE_TYPE, ResourceLocation.tryParse("blossom:flowering_oak_leaves"), FLOWERING_OAK_LEAVES);
-    }
 
 }
--- src/main/java/dev/yurisuika/blossom/world/level/item/BlossomItems.java
+++ src/main/java/dev/yurisuika/blossom/world/level/item/BlossomItems.java
@@ -1,20 +1,15 @@
 package dev.yurisuika.blossom.world.level.item;
 
 import dev.yurisuika.blossom.world.level.block.BlossomBlocks;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 
 public class BlossomItems {
 
-    public static final Item FRUITING_OAK_LEAVES = new BlockItem(BlossomBlocks.FRUITING_OAK_LEAVES, new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS));
-    public static final Item FLOWERING_OAK_LEAVES = new BlockItem(BlossomBlocks.FLOWERING_OAK_LEAVES, new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS));
-
-    public static void register() {
-        Registry.register(Registry.ITEM, ResourceLocation.tryParse("blossom:fruiting_oak_leaves"), FRUITING_OAK_LEAVES);
-        Registry.register(Registry.ITEM, ResourceLocation.tryParse("blossom:flowering_oak_leaves"), FLOWERING_OAK_LEAVES);
-    }
+    public static final Item FLOWERING_OAK_LEAVES = new BlockItem(BlossomBlocks.FLOWERING_OAK_LEAVES, new Item.Properties()
+            .tab(CreativeModeTab.TAB_DECORATIONS));
+    public static final Item FRUITING_OAK_LEAVES = new BlockItem(BlossomBlocks.FRUITING_OAK_LEAVES, new Item.Properties()
+            .tab(CreativeModeTab.TAB_DECORATIONS));
 
 }
